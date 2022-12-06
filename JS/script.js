ae3b66235d061dc281fb9152a12b987d64dcb189
Chubaj Vova <voovagame10-YT@yandex.ru>
2022-12-06 21:17:37 +0300

    Update

--- JS/script.js
+++ JS/script.js
@@ -1,44 +1,38 @@
-// Берём элемент для вывода таймера
-let timer_show = document.getElementById("timer");
- 
-// Функция для вычисления разности времени
-function diffSubtract(date1, date2) {
-    return date2 - date1;
-}
- 
-// Массив данных о времени
-let end_date = {
-    "full_year": "1970", // Год
-    "month": "01", // Номер месяца
-    "day": "01", // День
-    "hours": "00", // Час
-    "minutes": "00", // Минуты
-    "seconds": "00" // Секунды
-}
- 
-// Строка для вывода времени
-let end_date_str = `${end_date.full_year}-${end_date.month}-${end_date.day}T${end_date.hours}:${end_date.minutes}:${end_date.seconds}`;
-
-// Запуск интервала таймера
-timer = setInterval(function () {
-    // Получение времени сейчас
-    let now = new Date();
-    // Получение заданного времени
-    let date = new Date(end_date_str);
-    // Вычисление разницы времени 
-    let ms_left = diffSubtract(now, date);
-    // Если разница времени меньше или равна нулю 
-    if (ms_left <= 0) { // То
-        // Выключаем интервал
-        clearInterval(timer);
-        // Выводим сообщение об окончание
-        alert("Время закончилось");
-    } else { // Иначе
-        // Получаем время зависимую от разницы
-        let res = new Date(ms_left);
-        // Делаем строку для вывода
-        let str_timer = `${res.getUTCFullYear() - 1970}.${res.getUTCMonth()}.${res.getUTCDate() - 1} ${res.getUTCHours()}:${res.getUTCMinutes()}:${res.getUTCSeconds()}`;
-        // Выводим время
-        timer_show.innerHTML = str_timer;
+document.addEventListener('DOMContentLoaded', function() {
+    // конечная дата, например 1 июля 2021
+    const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 01);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
     }
-}, 1000)
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+      $days.dataset.title = declensionNum(days, ['день', 'дня', 'дней']);
+      $hours.dataset.title = declensionNum(hours, ['час', 'часа', 'часов']);
+      $minutes.dataset.title = declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      $seconds.dataset.title = declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
